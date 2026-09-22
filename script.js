30905ce26b1b542a23e6899e036ef2680711034b
Yana_Lusevich <preety_lunx@mail.ru>
2026-09-22 13:52:55 +0300

    feat: add new class and scripts

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 /* Switch theme */
 
+// Определяем базовый путь к папке images
+const isMenuPage = window.location.pathname.includes('page_menu');
+const basePath = isMenuPage ? '../images' : './images';
+
 const light = document.querySelector('.sun'),
     dark = document.querySelector('.moon'),
     img_sun = document.querySelector('.theme-switch-sun'),
@@ -8,34 +12,74 @@ const light = document.querySelector('.sun'),
     menu_icon_cup = document.querySelector('.menu-icon'),
     enjoy_btn = document.querySelector('.main-block_btn'),
     mobile_img = document.querySelector('.mobile-content_right img'),
-    sliderArrows = document.querySelectorAll('.slider-arrow');
+    sliderArrows = document.querySelectorAll('.slider-arrow'),
+    twitter = document.querySelector('.twitter'),
+    instagram = document.querySelector('.instagram'),
+    facebook = document.querySelector('.facebook'),
+    appStore = document.querySelector('.app-store'),
+    googlePlay = document.querySelector('.google-play'),
+    btnCoffee = document.querySelector('.btn-coffee'),
+    btnTea = document.querySelector('.btn-tea'),
+    btnDessert = document.querySelector('.btn-dessert');
 
-light.addEventListener('click', () => {
-    document.body.classList.toggle('dark-bg');
+if (light) {
+    light.addEventListener('click', () => {
+        document.body.classList.toggle('dark-bg');
+        document.documentElement.classList.toggle('dark-bg');
 
-    if (document.body.classList.contains('dark-bg')) {
-        header_logo_img.src = './images/header/logo_dark.svg';
-        menu_icon_cup.src = './images/header/coffee-cup_dark.svg';
-        img_moon.src = './images/header/moon-dark.svg';
-        enjoy_btn.style.backgroundColor = '#403F3D';
-        enjoy_btn.style.color = '#E1D4C9';
-        mobile_img.src = './images/mobile_img/mobile-screens-dark.svg';
-        sliderArrows.forEach(arrow => {
-            arrow.style.backgroundColor = '#292826';
-            arrow.style.borderColor = '#C1B6AD';
-        });
-    } else {
-        header_logo_img.src = './images/header/logo.svg';
-        menu_icon_cup.src = './images/header/coffee-cup.svg';
-        img_moon.src = './images/header/moon.svg';
-        enjoy_btn.style.backgroundColor = '#E1D4C9';
-        enjoy_btn.style.color = '#403F3D';
-        mobile_img.src = './images/mobile_img/mobile-screens.svg';
-        slider_arrow.style.backgroundColor = '#E1D4C9';
-    }
+        if (document.body.classList.contains('dark-bg')) {
+            // Основные изображения
+            if (header_logo_img) header_logo_img.src = `${basePath}/header/logo_dark.svg`;
+            if (menu_icon_cup) menu_icon_cup.src = `${basePath}/header/coffee-cup_dark.svg`;
+            if (img_moon) img_moon.src = `${basePath}/header/moon-dark.svg`;
+            if (enjoy_btn) {
+                enjoy_btn.style.backgroundColor = '#403F3D';
+                enjoy_btn.style.color = '#E1D4C9';
+            }
+            if (mobile_img) mobile_img.src = `${basePath}/mobile_img/mobile-screens-dark.svg`;
+            
+            // Стрелки слайдера
+            sliderArrows.forEach(arrow => {
+                arrow.style.backgroundColor = '#292826';
+                arrow.style.borderColor = '#C1B6AD';
+            });
 
-    console.log('Кнопка нажата')
-})
+            // Кнопки загрузки (для темной темы используем светлые версии)
+            if (appStore) appStore.style.backgroundImage = `url(${basePath}/mobile_img/apple-light.svg)`;
+            if (googlePlay) googlePlay.style.backgroundImage = `url(${basePath}/mobile_img/google_play-light.svg)`;
+
+            //Смена иконок меню
+
+            btnCoffee.backgroundImage = 'url(./images/menu/icon-coffee-dark.svg)'
+        } else {
+            // Основные изображения
+            if (header_logo_img) header_logo_img.src = `${basePath}/header/logo.svg`;
+            if (menu_icon_cup) menu_icon_cup.src = `${basePath}/header/coffee-cup.svg`;
+            if (img_moon) img_moon.src = `${basePath}/header/moon.svg`;
+            if (enjoy_btn) {
+                enjoy_btn.style.backgroundColor = '#E1D4C9';
+                enjoy_btn.style.color = '#403F3D';
+            }
+            if (mobile_img) mobile_img.src = `${basePath}/mobile_img/mobile-screens.svg`;
+
+            //Смена иконок меню
+
+            btnCoffee.backgroundImage = 'url(./images/menu/icon-coffee.svg)'
+            
+            // Стрелки слайдера
+            sliderArrows.forEach(arrow => {
+                arrow.style.backgroundColor = '#E1D4C9';
+                arrow.style.borderColor = '#403F3D';
+            });
+
+            // Кнопки загрузки на светлую тему
+            if (appStore) appStore.style.backgroundImage = `url(${basePath}/mobile_img/apple.svg)`;
+            if (googlePlay) googlePlay.style.backgroundImage = `url(${basePath}/mobile_img/google_play.svg)`;
+        }
+
+        console.log('Кнопка нажата')
+    });
+}
 
 /*Slider*/
 
